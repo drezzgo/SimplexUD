151a9596c1674d2066d852dc2f6a38f3ef307753
drezzgo <agongoraramirez@gmail.com>
2024-06-01 01:20:52 -0500

    Estandariza y separa los coeficientes, equidades y resultados de las funciones en arrays

--- public/js/graficoPython.js
+++ public/js/graficoPython.js
@@ -1,7 +1,7 @@
 let cantidadRestricciones = 3;
-let restricciones = {};
-let equivalenciasRestricciones = {};
-let resultadoRestricciones = {};
+let restricciones = [];
+let equivalenciasRestricciones = [];
+let resultadoRestricciones = [];
 
 let entrada = 'max = 22x1 + 45x2 \n1x1 - 3x2 <= 42\n1x1 + 2x2 <= 40\n0.5x1 + 1x2 <= 15';
 
@@ -9,14 +9,29 @@ let partes = entrada.trim().split('\n');
 console.log(partes);
 
 for (let i = 0; i < partes.length; i++) {
-    partes[i] = partes[i].replace("max = ", "").replace("min = ", "").replace("x1", "").replace("x2", "").replace("- ", "-");
-    console.log("Parte "+i+" : "+partes[i])
+    partes[i] = partes[i].trim().replace("max = ", "").replace("min = ", "").replace("x1", "").replace("x2", "").replace("- ", "-").replace(" + ", " ");
+    console.log("Parte "+i+" : "+partes[i]);
 
-    partes[i] = partes[i].split(" ")
-    console.log("Parte "+i+" : "+partes[i]+"\n")
+    partes[i] = partes[i].split(" ");
+    console.log(partes[i]);
 
-    coeficienteX1 = int(partes[0])
+    let coeficienteX1 = parseFloat(partes[i][0]);
+    let coeficienteX2 = parseFloat(partes[i][1]);
+    restricciones.push([coeficienteX1, coeficienteX2]);
+    
+    let equivalencia;
+    let resultado;
+    if (i!=0) {
+        let equivalencia = partes[i][2];
+        let resultado = parseFloat(partes[i][3]);
+        equivalenciasRestricciones.push(equivalencia);
+        resultadoRestricciones.push(resultado);
+    }
 }
+
+console.log("Restricciones: ", restricciones);
+console.log("Equivalencias: ", equivalenciasRestricciones);
+console.log("Resultados: ", resultadoRestricciones);
 
 
 
