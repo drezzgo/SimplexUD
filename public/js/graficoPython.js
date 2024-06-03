d9f08acb688dd21203b7d998cc1203d57ee17ff0
drezzgo <agongoraramirez@gmail.com>
2024-06-03 18:02:43 -0500

    Agrego Js de metodo grafico (Santi)

--- public/js/graficoPython.js
+++ public/js/graficoPython.js
@@ -3,29 +3,45 @@ let restricciones = [];
 let equivalenciasRestricciones = [];
 let resultadoRestricciones = [];
 
-let entrada = 'max = 22x1 + 45x2 \n1x1 - 3x2 <= 42\n1x1 + 2x2 <= 40\n0.5x1 + 1x2 <= 15';
+let entrada = 'max = 22x1 + 45x2 \n1x1 - 3x2 <= 42\n- 8x2 <= 40\n0.5x1 + 1x2 <= 15';
+let entradaMin = 'min = 2000x1 + 2000x2 \n1x1 + 2x2 >= 80\n3x1 + 2x2 >= 160\n5x1 + 2x2 >= 200'
 
-let partes = entrada.trim().split('\n');
+let partes = entradaMin.trim().split('\n');
 console.log(partes);
 
 for (let i = 0; i < partes.length; i++) {
     partes[i] = partes[i].trim().replace("max = ", "").replace("min = ", "").replace("x1", "").replace("x2", "").replace("- ", "-").replace(" + ", " ");
-    console.log("Parte "+i+" : "+partes[i]);
 
     partes[i] = partes[i].split(" ");
-    console.log(partes[i]);
+    console.log("Parte "+(i+1)+" : "+partes[i]+"\t|\t"+partes[i].length);
 
-    let coeficienteX1 = parseFloat(partes[i][0]);
-    let coeficienteX2 = parseFloat(partes[i][1]);
-    restricciones.push([coeficienteX1, coeficienteX2]);
-    
     let equivalencia;
     let resultado;
-    if (i!=0) {
-        let equivalencia = partes[i][2];
-        let resultado = parseFloat(partes[i][3]);
+
+    if (i == 0 && partes[i].length == 2 && ( !(Math.sign(partes[i][0]) < 0 && Math.sign(partes[i][1]) < 0) ) ){ //Funcion objetivo
+        let coeficienteX1 = parseFloat(partes[i][0]);
+        let coeficienteX2 = parseFloat(partes[i][1]);
+        restricciones.push([coeficienteX1, coeficienteX2]);
+
+    }else if(i != 0 && partes[i].length == 3 ){ //Restricciones solo con 1 termino
+        let coeficienteX1 = parseFloat(partes[i][0]);
+        let equivalencia = partes[i][1];
+        let resultado = parseFloat(partes[i][2]);
+        restricciones.push([coeficienteX1]);
         equivalenciasRestricciones.push(equivalencia);
         resultadoRestricciones.push(resultado);
+
+    } else if (i != 0 && partes[i].length == 4) { //Restricciones solo con 2 terminos
+        let coeficienteX1 = parseFloat(partes[i][0]);
+        let coeficienteX2 = parseFloat(partes[i][1]);
+        let equivalencia = partes[i][2];
+        let resultado = parseFloat(partes[i][3]);
+        restricciones.push([coeficienteX1, coeficienteX2]);
+        equivalenciasRestricciones.push(equivalencia);
+        resultadoRestricciones.push(resultado);
+
+    } else { //Si tienen menos de 1 termino o mas de 2, esta mal formulado
+        console.log("Vos sos re estupido, mira bien las restriccion "+(i+1));
     }
 }
 
